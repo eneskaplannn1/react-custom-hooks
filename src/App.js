891641286c05d288129d4d9985d441dcaeb3d61f
eneskaplannn1 <muhammedenes.kaplan@std.yeditepe.edu.tr>
2023-05-03 11:03:31 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const { isLoading, error, sendRequest } = useHttp();
+
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+      setTasks(loadedTasks);
+    };
+
+    sendRequest(
+      {
+        url: "https://learning-javascript-react-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTasks
+    );
+    //! bu örnekte sendRequest her useHttp fonksiyonu çalıştığı halde yeniden oluşturulup değeri değiştiğine göre neden //infinite loop oluşmuyor  ya da oluşuyor mu ?
+  }, [sendRequest]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={sendRequest}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
